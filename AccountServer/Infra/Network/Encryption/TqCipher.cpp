641dd619e7fcab3ea986957f9b7c6ecef8201991
Mahmoud Abdullah <p.mahmoudabdullah@gmail.com>
2023-03-30 17:07:31 +0200

    Encryption Fixed a NASTY bug in the TQ cipher

--- AccountServer/Infra/Network/Encryption/TqCipher.cpp
+++ AccountServer/Infra/Network/Encryption/TqCipher.cpp
@@ -33,40 +33,40 @@ namespace Eternal
         //004BBC59 encrypt
         void TqCipher::encrypt(std::uint8_t* _pBuf, size_t _len)
         {
-            for (size_t i{}; i < _len; i++)
+            uint8_t a = 0;
+            for (size_t i = 0; i < _len; ++i)
             {
+                _pBuf[i] ^= UINT8_C(0xAB);
+                _pBuf[i] = (_pBuf[i] << 0x4) + (_pBuf[i] >> 0x4);   
                 _pBuf[i] ^= _iv[_en_counter++];
-                _pBuf[i] ^= _iv[_de_counter + 0x100];
-                _pBuf[i] = (_pBuf[i] << 0x4) + (_pBuf[i] >> 0x4);
-                _pBuf[i] ^= 0xab;
+                _pBuf[i] ^= _iv[a + 0x100];
 
                 if (_en_counter > 0xff) {
                     _en_counter = 0;
-                    _de_counter++;
-                    if (_de_counter > 0xff)
-                        _de_counter = 0;
+                    a++;
+                } if (a > 0xff) {
+                    a = 0;
                 }
+
             }
         }
 
         void TqCipher::decrypt(uint8_t* _pBuf, size_t _len)
         {
-            uint8_t* key1 = _iv;
-            uint8_t* key2 = key1 + (TqCipher::IV_SIZE / 2);
-
+            uint8_t b = 0;
             for (size_t i = 0; i < _len; ++i)
             {
                 _pBuf[i] ^= UINT8_C(0xAB);
                 _pBuf[i] = (_pBuf[i] << 0x4) + (_pBuf[i] >> 0x4);    // the right shift operation is AFFECTED by the sign of its operand (char/uint8_t) 
                                                                      // wasted  ~4 hours on this little bug :))
                 _pBuf[i] ^= _iv[_de_counter++];
-                _pBuf[i] ^= _iv[_en_counter + 0x100];
+                _pBuf[i] ^= _iv[b + 0x100];
 
                 if (_de_counter > 0xff) {
                     _de_counter = 0;
-                    _en_counter++;
-                    if (_en_counter > 0xff)
-                        _en_counter = 0;
+                    b++;
+                    if (b > 0xff)
+                         b = 0;
                 }
             }
         }
