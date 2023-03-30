99e8f5573b6ef8465ca4dea9670cbbac74013e34
Mahmoud Abdullah <p.mahmoudabdullah@gmail.com>
2023-03-30 22:41:20 +0200

    Files renamed AccountMain.cpp to main.cpp as it's easier to find in the solution explorer

---
+++ AccountServer/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "./Infra/Network/Server.h"
+#include "./Infra/Network/Connection.h"
+#include "./Infra/Network/Encryption/TqCipher.h"
+#include <string>
+#include <thread>
+#include <Windows.h>
+
+int main()
+{
+	try {
+		Eternal::Server AccountServer("127.0.0.1", 55099);
+		AccountServer._on_accept = [&](std::shared_ptr<Eternal::Connection> connection) {
+			auto tq_cipher = std::make_unique<Eternal::Encryption::TqCipher>();
+			tq_cipher->generate_iv();
+			connection->set_cipher(std::move(tq_cipher));
+			};
+
+		AccountServer._on_receive = [&](std::shared_ptr<Eternal::Connection> connection, size_t bytes_received) {
+			// TODO: might want to move it to connection.receive()
+			auto data = connection->get_buffer().get();
+			connection->get_cipher()->decrypt(data, bytes_received);
+			auto msg = Eternal::Msg::NetMsg::create(connection->get_buffer(), bytes_received);	
+			msg->process(AccountServer);
+		};
+
+		AccountServer.take_over();
+		//AccountServer.set_threads(3).run();
+
+		while (!(GetAsyncKeyState(VK_NUMPAD0) & 1))
+		{
+			Sleep(100);
+		}
+
+		AccountServer.shutdown();
+		std::cout << "Server stopped\n";
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Exception: " << e.what() << '\n';
+	}
+
+}
