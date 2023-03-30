1a42bec7a42fb856b904b8a5e189dc22b58c4ccb
Mahmoud Abdullah <p.mahmoudabdullah@gmail.com>
2023-03-30 22:40:47 +0200

    Msg Implemented MsgConnectEx and MsgConnect

---
+++ AccountServer/Msg/MsgConnectEx.h
@@ -0,0 +1,41 @@
+#pragma once
+#include "NetMsg.h"
+
+namespace Eternal
+{
+    namespace Msg
+    {
+        class MsgConnectEx : public NetMsg
+        {
+        private:
+#pragma pack(push, 1)
+            struct Info
+            {
+                Msg::Header header;
+                int32_t client_identity;
+                int32_t authentication_code;
+                char game_server_ip[16];
+                int32_t game_server_port;
+
+            };
+#pragma pack(pop)
+        public:
+            MsgConnectEx(std::shared_ptr<uint8_t[]>&& data, size_t len);
+            MsgConnectEx();
+
+        public:
+            MsgConnectEx& set_client_identity(int32_t identity);
+            MsgConnectEx& set_authentication_code(int32_t auth_code);
+            MsgConnectEx& set_game_server_ip(const char* ip);
+            MsgConnectEx& set_game_server_port(int32_t port);
+
+
+
+        private:
+            Info* _info;
+        };
+
+    }
+}
+
+
