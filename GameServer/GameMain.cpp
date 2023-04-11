77967438a95c0fb9202406eca9dfb32536942649
Mahmoud Abdullah <p.mahmoudabdullah@gmail.com>
2023-04-11 14:39:31 +0200

    Msg The KEY_EXCHANGE process is completed.

--- GameServer/GameMain.cpp
+++ GameServer/GameMain.cpp
@@ -9,6 +9,7 @@
 #include <string>
 #include <thread>
 #include <Windows.h>
+#include "Network/Encryption/IExchange.h"
 
 int main()
 {
@@ -16,45 +17,62 @@ int main()
 		Eternal::Server GameServer("127.0.0.1", 5816);
 
 		GameServer._on_accept = [&](std::shared_ptr<Eternal::Connection> connection) {
-			// TODO: implement the Blowfish and DH exhange
-			// MsgLoginProofA -> (BF & DH)
+
 			auto bf = std::make_unique<Blowfish>("DR654dt34trg4UI6");
 			connection->set_cipher(std::move(bf));
-
 			const std::string P{ "E7A69EBDF105F2A6BBDEAD7E798F76A209AD73FB466431E2E7352ED262F8C558F10BEFEA977DE9E21DCEE9B04D245F300ECCBBA03E72630556D011023F9E857F" };
 			const std::string G{ "05" };
-			DiffieHellman dh(P, G);
-			dh.generate_key();
-			std::cout << dh.get_private_key() << '\n';
-			std::cout << dh.get_public_key()  << " - " << dh.get_public_key().size() << '\n';
-
-			auto msg = std::make_shared<MsgLoginProofA>( P, G, dh.get_public_key() );
+			std::unique_ptr<Eternal::Encryption::IExchange> dh(new DiffieHellman(P, G));
+			dh->generate_key();
+			auto msg = std::make_shared<MsgLoginProofA>( P, G, dh->get_public_key() );
 			GameServer.queue_msg(msg);
-
-			/*auto tq_cipher = std::make_unique<Eternal::Encryption::TqCipher>();
-			tq_cipher->generate_iv();
-			connection->set_cipher(std::move(tq_cipher));
-			std::shared_ptr<Eternal::Msg::MsgConnectEx> test = std::make_shared<Eternal::Msg::MsgConnectEx>();
-			test->set_authentication_code(10).set_client_identity(100).set_game_server_ip("192.192.192.1").set_game_server_port(0x1337);
-			GameServer.queue_msg(test);*/
+			connection->set_exchange(std::move(dh));
+			connection->set_state(Eternal::Connection::State::KEY_EXCHANGE);
 		};
 
 		GameServer._on_receive = [&](std::shared_ptr<Eternal::Connection> connection, size_t bytes_received) {
-			// TODO: might want to move it to connection.receive()
-			auto data = connection->get_buffer().get();
-			connection->get_cipher()->decrypt(data, bytes_received);
-			auto msg = Eternal::Msg::NetMsg::create(connection->get_buffer(), bytes_received);
-			msg->process(GameServer);
+			switch (connection->get_state())
+			{
+			case Eternal::Connection::State::KEY_EXCHANGE:
+			{
+				auto& cipher = connection->get_cipher();
+				auto buffer = connection->get_buffer();
+				cipher->decrypt(buffer.get(), bytes_received);
+				// parse the incoming response
+				// TODO: Move the handling part to a different Object?
+				uint8_t* ptr = buffer.get();
+				ptr += UINT8_C(7);			// skip padding
+				uint32_t incoming_size = *((uint32_t*)ptr);
+				ptr += sizeof(uint32_t);	// skip incoming_size
+				uint32_t junk_size = *((uint32_t*)ptr);
+				ptr += junk_size ;
+				ptr += sizeof(uint32_t);	// skip the junk_size
+				uint32_t client_public_len = *((uint32_t*)ptr);
+				ptr += sizeof(uint32_t);	// skip the client_public_len
+				auto client_public = std::string_view((char*)ptr, client_public_len);
+				auto& exchange = connection->get_exchange();
+				auto shared_secret = exchange->derive_shared_secret(client_public);
+				auto bf = std::make_unique<Blowfish>(shared_secret);
+				connection->set_cipher(std::move(bf));
+				connection->set_state(Eternal::Connection::State::NORMAL);
+				break;
+			}
+			case Eternal::Connection::State::NORMAL:
+			{
+				auto data = connection->get_buffer().get();
+				connection->get_cipher()->decrypt(data, bytes_received);
+				auto msg = Eternal::Msg::NetMsg::create(connection->get_buffer(), bytes_received);
+				msg->process(GameServer);
+				break;
+			}
+			}
 		};
 
 		GameServer.take_over();
-		//GameServer.set_threads(3).run();
-
 		while (!(GetAsyncKeyState(VK_NUMPAD0) & 1))
 		{
 			Sleep(100);
 		}
-
 		GameServer.shutdown();
 		std::cout << "Server stopped\n";
 	}
